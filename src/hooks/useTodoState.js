789c34190dfbcd7c367067646e4c6921ee5bbf46
Spencer Haugh <spencer.haugh@gmail.com>
2021-06-10 15:16:19 -0400

    Update uuid

--- src/hooks/useTodoState.js
+++ src/hooks/useTodoState.js
@@ -1,5 +1,5 @@
 import useLocalStorageState from './useLocalStorageState';
-import uuid from 'uuid/v4';
+import { v4 as uuid } from 'uuid';
 
 const useTodoState = initialTodos => {
     const [todos, setTodos] = useLocalStorageState("todos", initialTodos);
